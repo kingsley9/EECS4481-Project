dd921786ca879f7a542703dcff67c7cca1b6876a
kingsley <king808@my.yorku.ca>
2023-02-24 10:53:30 -0500

    add icons

--- client-app/src/pages/home/home.tsx
+++ client-app/src/pages/home/home.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './home.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComments, faUserTie } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
   return (
@@ -8,9 +10,11 @@ const Home = () => {
       <h1>Help Desk App</h1>
       <div className="home-buttons-container">
         <Link to="/dashboard" className="home-button">
+          <FontAwesomeIcon icon={faComments} size="3x" />
           Get Support
         </Link>
         <Link to="/admin/dashboard" className="home-button">
+          <FontAwesomeIcon icon={faUserTie} size="3x" />
           Admin Dashboard
         </Link>
       </div>
