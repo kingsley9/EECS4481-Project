170c271117a80eab091090f878f317ae239ae7bb
kingsley <king808@my.yorku.ca>
2023-02-24 16:03:52 -0500

    add admin dashboard with chat box and styles

--- client-app/src/pages/admin/admin-dashboard.tsx
+++ client-app/src/pages/admin/admin-dashboard.tsx
@@ -4,11 +4,17 @@ import { API_URL } from '../../config/default';
 import { useNavigate } from 'react-router-dom';
 import { verifyToken, logout } from '../../services/jwt'; // import logout from jwt service
 import axios from 'axios';
-
+import ChatBox from '../../components/chat-box';
+import './admin-dashboard.css';
 const AdminDashboard = () => {
   const navigate = useNavigate();
   const token = getCookie('token');
   const [adminMessage, setAdminMessage] = useState('');
+  const [conversations, setConversations] = useState([
+    { id: 2, name: 'user12134' },
+    { id: 3, name: 'user12244' },
+    { id: 4, name: 'user12264' },
+  ]);
 
   useEffect(() => {
     const checkToken = async () => {
@@ -21,7 +27,7 @@ const AdminDashboard = () => {
             const response = await axios.get(`${API_URL}/api/admin`, {
               headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `JWT ${token}`,
+                Authorization: `JWT ${token}`,
               },
             });
 
@@ -50,10 +56,25 @@ const AdminDashboard = () => {
   };
 
   return (
-    <div>
+    <div style={{ margin: 'auto' }}>
       <h1>Admin Dashboard</h1>
       <p>{adminMessage}</p>
-      <button onClick={handleLogout}>Logout</button>
+      <button className="logout-button" onClick={handleLogout}>
+        Logout
+      </button>
+      <div className="admin-dashboard">
+        <div className="admin-panel">
+          <h2 style={{ margin: '15px' }}>Conversations</h2>
+          <ul>
+            {conversations.map((conversation) => (
+              <li key={conversation.id}>{conversation.name}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="admin-content">
+          <ChatBox sessionId="token" />
+        </div>
+      </div>
     </div>
   );
 };
