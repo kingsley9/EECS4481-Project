3596a4b3b3600ddcd63064cb4220845e505ffd1a
Shahab Mouradzadegun <mouradzadegun@gmail.com>
2023-02-23 22:57:57 -0500

    updated constants

--- client-app/src/config/default.js
+++ client-app/src/config/default.js
@@ -1,4 +1,1 @@
 export const API_URL = 'http://localhost:8080';
-export const MESSAGES_ENDPOINT = '/messages';
-export const LOGIN_ENDPOINT = '/login';
-export const SEND_MESSAGE_ENDPOINT = '/message';
--- client-app/src/utils/constants.js
+++ client-app/src/utils/constants.js
@@ -1,4 +1,1 @@
 export const API_URL = 'https://localhost:8080/api';
-export const MESSAGES_ENDPOINT = '/messages';
-export const LOGIN_ENDPOINT = '/login';
-export const SEND_MESSAGE_ENDPOINT = '/message';
