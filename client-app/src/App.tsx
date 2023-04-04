7dd9d1e94a210092c842a4608dfcec5e349087c7
kingsley <king808@my.yorku.ca>
2023-04-04 04:04:28 -0400

    add cookie optin and style changes

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,27 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/home';
 import './app.css';
 import About from './pages/about/about';
 import Header from './components/common/header';
 import Footer from './components/common/footer';
+import CookieOptIn from './components/common/cookieOptIn';
 import Login from './pages/admin/login';
 import Dashboard from './pages/dashboard/dashboard';
 import AdminDashboard from './pages/admin/admin-dashboard';
+import Cookies from 'js-cookie';
 
 function App() {
+  const [showBanner, setShowBanner] = useState(false);
+
+  useEffect(() => {
+    const hasAllowedCookies = Cookies.get('allowCookies') === 'true';
+    if (!hasAllowedCookies) {
+      setShowBanner(true);
+    }
+  }, []);
+
+  const handleAllowCookies = () => {
+    Cookies.set('allowCookies', 'true', { expires: 365 });
+    setShowBanner(false);
+  };
+
+  const handleDeclineCookies = () => {
+    Cookies.set('allowCookies', 'false', { expires: 365 });
+    setShowBanner(false);
+    // Do something else here if the user declines cookies
+  };
+
   return (
     <Router>
       <Header />
       <div className="content">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+
+          {!showBanner && <Route path="/dashboard" element={<Dashboard />} />}
           <Route path="/about" element={<About />} />
-          <Route path="/admin/login" element={<Login />} />
-          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          {!showBanner && <Route path="/admin/login" element={<Login />} />}
+          {!showBanner && (
+            <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          )}
         </Routes>
       </div>
+
+      <CookieOptIn
+        showBanner={showBanner}
+        handleAllowCookies={handleAllowCookies}
+        handleDeclineCookies={handleDeclineCookies}
+      />
       <Footer />
     </Router>
   );
--- client-app/src/config/default.js
+++ client-app/src/config/default.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === 'development'
-    ? 'http://35.232.92.79:3100'
-    : 'https://help-desk-app-t5.herokuapp.com';
+    ? 'http://localhost:3100'
+    : 'http://35.232.92.79:3000';
