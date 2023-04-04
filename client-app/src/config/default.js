c2e9ed2eeb79e05732dd4c43b65bfdbc5b9ffe78
Shahab Mouradzadegun <mouradzadegun@gmail.com>
2023-04-04 05:39:22 -0400

    feat: update

--- client-app/src/config/default.js
+++ client-app/src/config/default.js
@@ -1,4 +1,4 @@
 export const API_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : 'http://35.232.92.79:3100';
+  process.env.NODE_ENV === 'production'
+    ? 'http://35.232.92.79:3100'
+    : 'http://localhost:3000';
