e449dcf8d24466387e0561794b97ca7f3f8ee7f6
Shahab Mouradzadegun <mouradzadegun@gmail.com>
2023-04-04 06:57:33 -0400

    message scroll

--- client-app/src/components/messages.tsx
+++ client-app/src/components/messages.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import './Messages.css';
 import { Message } from '../data/message';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -14,7 +14,9 @@ const Messages: React.FC<Props> = ({ messages, role, onFileDownload }) => {
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
 
   const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
   };
 
   useEffect(() => {
@@ -30,33 +32,32 @@ const Messages: React.FC<Props> = ({ messages, role, onFileDownload }) => {
         >
           <span>{message.sender}:&nbsp;</span>
           <span>{message.message}</span>
-          {message.files &&
-            message.files.map((file) => (
-              <div key={file.fileId} className="file-container">
-                {file.fileType.startsWith('image') ? (
-                  <img
-                    className="file-image"
-                    src={`/api/user/file/${file.fileId}`}
-                    alt={file.filename}
-                  />
-                ) : (
-                  <div className="file-image file-icon">
-                    <span className="file-extension">
-                      {file.fileType.split('/')[1]}
-                    </span>
-                  </div>
-                )}
-                <div className="file-details">
-                  <span className="file-name">{file.filename}</span>
-                  <button
-                    className="file-download"
-                    onClick={() => onFileDownload(file.fileId, file.filename)}
-                  >
-                    <FontAwesomeIcon icon={faDownload} />
-                  </button>
+          {message.files && message.files.map((file) => (
+            <div key={file.fileId} className="file-container">
+              {file.fileType.startsWith('image') ? (
+                <img
+                  className="file-image"
+                  src={`/api/user/file/${file.fileId}`}
+                  alt={file.filename}
+                />
+              ) : (
+                <div className="file-image file-icon">
+                  <span className="file-extension">
+                    {file.fileType.split('/')[1]}
+                  </span>
                 </div>
+              )}
+              <div className="file-details">
+                <span className="file-name">{file.filename}</span>
+                <button
+                  className="file-download"
+                  onClick={() => onFileDownload(file.fileId, file.filename)}
+                >
+                  <FontAwesomeIcon icon={faDownload} />
+                </button>
               </div>
-            ))}
+            </div>
+          ))}
         </div>
       ))}
       <div ref={messagesEndRef} />
